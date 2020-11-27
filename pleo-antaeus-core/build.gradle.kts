6f85a24b27a0867c06498566e81bcf4a11baa049
Wisdom Adzorgenu <wadzorgenu@gmail.com>
2020-11-27 19:35:55 +0000

    Added kotlinx coroutines to antaeus core module.

--- pleo-antaeus-core/build.gradle.kts
+++ pleo-antaeus-core/build.gradle.kts
@@ -7,5 +7,6 @@ kotlinProject()
 dependencies {
     implementation(project(":pleo-antaeus-data"))
     implementation("org.quartz-scheduler:quartz:2.3.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.2")
     api(project(":pleo-antaeus-models"))
 }
