972df44c8b99c6bb3ff4f380f1a38765103bae30
Wisdom Adzorgenu <wadzorgenu@gmail.com>
2020-11-27 19:38:04 +0000

    Added new methods to count pending invoices, fetch pending invoices in batches and update invoice payment.

--- pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/InvoiceService.kt
+++ pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/InvoiceService.kt
@@ -7,8 +7,21 @@ package io.pleo.antaeus.core.services
 import io.pleo.antaeus.core.exceptions.InvoiceNotFoundException
 import io.pleo.antaeus.data.AntaeusDal
 import io.pleo.antaeus.models.Invoice
+import io.pleo.antaeus.models.InvoiceStatus
 
 class InvoiceService(private val dal: AntaeusDal) {
+    fun countUnpaid():Int {
+        return dal.countUnpaidInvoices()
+    }
+
+    fun fetchUnpaidInBatches(lastInvoiceId:Int,limit:Int=1): List<Invoice>{
+        return dal.fetchUnpaidInvoiceInBatches(lastInvoiceId,limit)
+    }
+
+    fun updatePaidStatus(invoiceId:Int):Any{
+        return dal.updateInvoicePaidStatus(invoiceId)
+    }
+
     fun fetchAll(): List<Invoice> {
         return dal.fetchInvoices()
     }
