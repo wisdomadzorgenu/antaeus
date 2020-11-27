3eebe2729dc7cb528a1b1a70cd6f38f3f12a8a28
Wisdom Adzorgenu <wadzorgenu@gmail.com>
2020-11-27 09:57:42 +0000

    Added quartz scheduler to antaeus core module

--- pleo-antaeus-core/build.gradle.kts
+++ pleo-antaeus-core/build.gradle.kts
@@ -6,5 +6,6 @@ kotlinProject()
 
 dependencies {
     implementation(project(":pleo-antaeus-data"))
+    implementation("org.quartz-scheduler:quartz:2.3.0")
     api(project(":pleo-antaeus-models"))
 }
